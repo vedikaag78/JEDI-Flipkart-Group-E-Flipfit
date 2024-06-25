c2700383ff16af3f1a9b53e7862cafa31caec8bc
Rahul Ketan  Modi <rahul_km@ee.iitr.ac.in>
2024-06-26 00:31:41 +0530

    Structure Created

---
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/bean/Admin.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.flipkart.bean;
+
+/**
+ * 
+ */
+public class Admin {
+	public int getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+	public String getAdminName() {
+		return adminName;
+	}
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+	public int getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(int contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	private int adminId;
+	private String adminName;
+	private int contactNumber;
+}
--- JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/business/CustomerBusiness.java
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/business/CustomerBusiness.java
@@ -7,5 +7,21 @@ package com.flipkart.business;
  * 
  */
 public class CustomerBusiness {
-
+	public void createCustomer() {
+		System.out.println("Customer  Created");
+	}
+	
+	public boolean updateCustomer(int customerId) {
+		System.out.println("Customer Updated ==> " + customerId);
+		return true;
+	}
+	
+	public boolean deleteCustomer(int customerId) {
+		System.out.println("Customer Deleted ==> " + customerId);
+		return true;
+	}
+	
+	public void listCustomer() {
+		System.out.println("Get all Customer List");
+	}
 }
