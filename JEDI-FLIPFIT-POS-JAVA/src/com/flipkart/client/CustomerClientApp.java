93eeb8d80f895fee29449e989231e656fa600141
vedikaag78 <vedika1510@gmail.com>
2024-06-25 21:38:04 +0530

    first

---
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/client/CustomerClientApp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.flipkart.client;
+
+/**
+ * 
+ */
+public class CustomerClientApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
