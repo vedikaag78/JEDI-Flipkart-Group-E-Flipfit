8c7623f5be44a93d804d233808ec1abeabcbad56
vedikaag78 <vedika1510@gmail.com>
2024-06-26 12:05:14 +0530

    booking business

--- JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/business/BookingBusiness.java
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/business/BookingBusiness.java
@@ -3,9 +3,22 @@
  */
 package com.flipkart.business;
 
+import java.sql.Date;
+
 /**
  * 
  */
 public class BookingBusiness {
+	public boolean checkBooking(String customerId, Date date, String slotId){
+        return true;
+    }
+    public void addBooking(String userName, Date date) {
+    	
+    }
 
+    public void getBookingByCustomerId(String customerId){
+     
+    }
+    public void cancelBooking(String bookingID) {
+    }
 }
--- JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/business/BookingInterface.java
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/business/BookingInterface.java
@@ -3,9 +3,16 @@
  */
 package com.flipkart.business;
 
+import java.sql.Date;
+
 /**
  * 
  */
 public interface BookingInterface {
-
+	
+	public boolean checkBooking(String customerId, Date date, String slotId);
+    public void addBooking(String userName, Date date ) ;
+    public void getBookingByCustomerId(String customerId);
+    public void cancelBooking(String bookingID);
+	
 }
