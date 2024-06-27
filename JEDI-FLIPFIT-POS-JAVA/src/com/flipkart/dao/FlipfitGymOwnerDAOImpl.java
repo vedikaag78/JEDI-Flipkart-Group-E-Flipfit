2f372e591330e0f33e3ee75405aa3d0667ab324f
Aniket Kumar <fabaniket246@gmail.com>
2024-06-28 00:39:18 +0530

    daoImpl

--- JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/dao/FlipfitGymOwnerDAOImpl.java
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/dao/FlipfitGymOwnerDAOImpl.java
@@ -4,22 +4,29 @@
 package com.flipkart.dao;
 import com.flipkart.bean.GymCenter;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /*
- Remaining :
+Functions Implemented :
+1. addGymDetails
+2. updateGymDetails
+3. getAllGymCCenters
+4. deleteGymDetails
+ */
+/*
+ Note :
  1) Rewrite GYM_TABLE as the gym_table used in db
  2) Rewrite the relevant password
  */
 
-public class FlipfitGymOwnerDAOImpl implements FlipfitGymOwnerDAOInterface{
+public abstract class FlipfitGymOwnerDAOImpl implements FlipfitGymOwnerDAOInterface{
     private static final String DB_URL = "jdbc:mysql://localhost:3306/flipfit_schema";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "12345678";
-    private static final String GYM_TABLE = "gym_details";
+    private static final String GYM_TABLE = "GymCenters";
 
 
     public void addGymDetails(GymCenter gymCenter) {
@@ -28,7 +35,7 @@ public class FlipfitGymOwnerDAOImpl implements FlipfitGymOwnerDAOInterface{
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/flipfit_schema", "root", "12345678");
-            PreparedStatement stmt = con.prepareStatement("insert into tablename values(?,?,?,?,?,?,?,?)");
+            PreparedStatement stmt = con.prepareStatement("insert into GymCenters values(?,?,?,?,?,?,?)");
             stmt.setInt(1, gymCenter.getGymCenterId());
             stmt.setInt(2,gymCenter.getGymOwnerId() );
             stmt.setString(3,gymCenter.getGymCenterName() );
@@ -36,13 +43,7 @@ public class FlipfitGymOwnerDAOImpl implements FlipfitGymOwnerDAOInterface{
             stmt.setString(5,gymCenter.getCity() );
             stmt.setInt(6, gymCenter.getPrice());
             stmt.setInt(7, gymCenter.getCapacity());
-          /* Logic to add slot ids as input
-          for (int i = 0 ; i < gymCenter.getSlotListIds().size(); i++) {
 
-          }
-
-          stmt.setArrayList(8 , gymCenter.setSlotListIds());
-           */
 
             int i = stmt.executeUpdate();
             System.out.println(i + " records inserted");
@@ -67,15 +68,44 @@ public class FlipfitGymOwnerDAOImpl implements FlipfitGymOwnerDAOInterface{
             stmt.setInt(5, gymCenter.getPrice());
             stmt.setInt(6, gymCenter.getCapacity());
             stmt.setInt(7, gymCenter.getGymCenterId());
-/*
-Logic to update Array of slots
- */
+
             int i = stmt.executeUpdate();
             System.out.println(i + " records updated");
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public List<GymCenter> getAllGymCenters(int gymOwnerId) {
+
+        List<GymCenter> gymCenters = new ArrayList<>();
+        String selectQuery = "SELECT * FROM " + GYM_TABLE + " WHERE gymOwnerId = ?";
+        try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement stmt = con.prepareStatement(selectQuery)) {
+
+            stmt.setInt(1, gymOwnerId);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    GymCenter gymCenter = new GymCenter();
+                    gymCenter.setGymCenterId(rs.getInt("gymCenterId"));
+                    gymCenter.setGymOwnerId(rs.getInt("gymOwnerId"));
+                    gymCenter.setGymCenterName(rs.getString("gymCenterName"));
+                    gymCenter.setAddress(rs.getString("address"));
+                    gymCenter.setCity(rs.getString("city"));
+                    gymCenter.setPrice(rs.getInt("price"));
+                    gymCenter.setCapacity(rs.getInt("capacity"));
+
+                    gymCenters.add(gymCenter);
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return gymCenters;
+
     }
 
 
@@ -95,14 +125,6 @@ Logic to update Array of slots
             e.printStackTrace();
         }
     }
-
-    public void addSlots (GymCenter gymCenter) {
-        
-
-    }
-
-
-
 
 
 
--- JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/dao/FlipfitGymOwnerDAOInterface.java
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/dao/FlipfitGymOwnerDAOInterface.java
@@ -3,9 +3,15 @@
  */
 package com.flipkart.dao;
 
+import com.flipkart.bean.GymCenter;
+
 /**
  * 
  */
 public interface FlipfitGymOwnerDAOInterface {
+    public void addGymDetails(GymCenter gymCenter) ;
+    public void updateGymDetails(GymCenter gymCenter);
+    public void deleteGymDetails(GymCenter gymCenter);
+    public GymCenter getGymDetails(GymCenter gymCenter);
 
 }
